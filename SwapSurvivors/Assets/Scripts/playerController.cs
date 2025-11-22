c1e7e701a535a9a154bf8b652fad34e0989431b0
Galip Efe Öncü <galipefe75@gmail.com>
2025-11-22 12:39:20 +0300

    player

---
+++ SwapSurvivors/Assets/Scripts/playerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    InputActions controls;
+
+    [SerializeField] private float playerSpeed = 5.0f;
+    Vector2 moveInput;
+
+    private void OnEnable() => controls.Player.Enable();
+    private void OnDisable() => controls.Player.Disable();
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        controls = new InputActions();
+
+        controls.Player.Move.performed += ctx => { moveInput = ctx.ReadValue<Vector2>(); };
+        controls.Player.Move.canceled += ctx => { moveInput = Vector2.zero; };
+    }
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * playerSpeed, moveInput.y * playerSpeed);
+    }
+}
